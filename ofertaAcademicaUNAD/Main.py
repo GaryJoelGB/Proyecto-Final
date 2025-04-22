de0b4c5459f49d0dba57bbf28189d3cec4ea295f
Gary Joel <102198681+GaryJoelGB@users.noreply.github.com>
2025-04-22 14:21:33 -0400

    Add files via upload

---
+++ ofertaAcademicaUNAD/Main.py
@@ -0,0 +1,33 @@
+# main.py
+import ofertaAcademica
+import consultaCarrera
+import consultaPostgrado
+
+def mostrar_menu():
+    print("\n========== MENÚ DE CONSULTA ==========\n")
+    print("1. Ver Carreras de Grado")
+    print("2. Ver Carreras de Postgrado")
+    print("3. Ver Facultades")
+    print("4. Consultar Informacion de una Carrera de Grado")
+    print("5. Consultar Informacion de una Carrera de Post Grado")
+    print("6. Salir del Programa")
+
+while True:
+    mostrar_menu()
+    opcion = input("\n Selecciona una opción (1-6): ")
+
+    if opcion == "1":
+         ofertaAcademica.buscarCarrerasGrado()
+    elif opcion == "2":
+        ofertaAcademica.buscarCarrerasPostGrado()
+    elif opcion == "3":
+        ofertaAcademica.buscarFacultad()
+    elif opcion == "4":
+        consultaCarrera.consultarCarreras()
+    elif opcion == "5":
+        consultaPostgrado.consultarPostgrados()
+    elif opcion == "6":
+        print("Saliendo del programa.")
+        break
+    else:
+        print("Opción no válida. Intenta nuevamente.")
